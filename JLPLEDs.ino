899995569fcf101be416830081345ad7eb01207e
joshua-8 <joshua_phelps@brown.edu>
2022-01-16 12:04:03 -0800

    more patterns

--- JLPLEDs.ino
+++ JLPLEDs.ino
@@ -1,6 +1,7 @@
 /*
    January 2022:
-      found coordinates of each light and programmed first patterns
+      found coordinates of each light
+      programmed first patterns
 */
 #include <FastLED.h>
 #define DATA_PIN 2
@@ -12,6 +13,9 @@
 #define led leds[L]
 #define coordx (coords[L][x] - cx)
 #define coordy (coords[L][y] - cy)
+#define letterJ (coordx + cx < 80)
+#define letterL (coordx + cx >= 80 && coordx + cx < 150)
+#define letterP (coordx + cx >= 150)
 #include "coords.h"
 CRGB leds[lct];
 byte L;
@@ -31,11 +35,40 @@ void loop() {
     //    //inward rainbow circle
     //    leds[L] = CHSV(millis() / 8 + 3 * sqrt(sq(coordx) + sq(coordy)), 255, 100);
 
+    //    //outward rainbow circle
+    //    leds[L] = CHSV(millis() / 8 - 3 * sqrt(sq(coordx) + sq(coordy)), 255, 100);
+
     //    //inward hard edge circle
     //    if (((byte)(millis() / 2 + 3 * sqrt(sq(coordx) + sq(coordy)))) < 20) {
     //      leds[L] = CRGB(255, 0, 255);
     //    } else {
     //      leds[L] = CRGB(0, 0, 0);
+    //    }
+
+    //    //outward hard edge circle
+    //    if (((byte)(millis() / 2 - 3 * sqrt(sq(coordx) + sq(coordy)))) < 20) {
+    //      leds[L] = CRGB(255, 0, 255);
+    //    } else {
+    //      leds[L] = CRGB(0, 0, 0);
+    //    }
+
+    //    inward circle
+    //    byte d = ((byte)(millis() / 2 + 3 * sqrt(sq(coordx) + sq(coordy))));
+    //    if (d < 30) {
+    //      leds[L] = CRGB(150, (15 - abs(d - 15)) * 17, 255 );
+    //    } else {
+    //      leds[L] = CRGB(150, 0, 255);
+    //    }
+
+    //    //outward rainbow circle
+    //    leds[L] = CHSV(millis() / 8 - 3 * sqrt(sq(coordx) + sq(coordy)), 255, 100);
+
+    //    //outward circle
+    //    byte d = ((byte)(millis() / 2 - 3 * sqrt(sq(coordx) + sq(coordy))));
+    //    if (d < 30) {
+    //      leds[L] = CRGB(150, (15 - abs(d - 15)) * 17, 255 );
+    //    } else {
+    //      leds[L] = CRGB(150, 0, 255);
     //    }
 
     //    //colorwheel
@@ -46,7 +79,7 @@ void loop() {
     //    v = v < 25 ? 250 - v * 10 : 0;
     //    leds[L] = CHSV(0, 0, v);
 
-    //    //    radar screen
+    //    //radar screen
     //    byte v = (millis() / 10 + 255 * (PI + atan2(coordy, coordx)) / TWO_PI);
     //    if (v < 1) {
     //      pixelTime[L] = millis();
@@ -59,8 +92,7 @@ void loop() {
     //      leds[L] = CRGB(v, v, v);
     //    }
 
-
-    //    //  4 direction wipers
+    //    //4 direction wipers
     //    static CRGB color;
     //    int t = millis() % 4000;
     //    int d = 0;
@@ -89,6 +121,21 @@ void loop() {
     //      led = color;
     //    } else {
     //      led = CRGB(0, 0, 0);
+    //    }
+
+    //    //color by letters with sparkles
+    //    if (random(200) == 0) {
+    //      led = CRGB(255, 255, 255);
+    //    } else {
+    //      if (letterJ) {
+    //        led = CRGB(255, 0, 0);
+    //      }
+    //      if (letterL) {
+    //        led = CRGB(255, 0, 255);
+    //      }
+    //      if (letterP) {
+    //        led = CRGB(0, 0, 255);
+    //      }
     //    }
 
   } // end of led iteration for loop
