e3650f799759feaf67b3dfa7da366404c75da6b7
joshua-8 <joshua_phelps@brown.edu>
2022-01-15 21:07:18 -0800

    two patterns working!

--- JLPLEDs.ino
+++ JLPLEDs.ino
@@ -1,9 +1,12 @@
 #include <FastLED.h>
 #define lct 120
+#define x 0
+#define y 1
+#define cx 122
+#define cy 90
 #define DATA_PIN 2
+#include "coords.h"
 CRGB leds[lct];
-
-int L = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -12,10 +15,19 @@ void setup() {
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, lct);  // GRB ordering is typical
 }
 void loop() {
-  if (Serial.available()) {
-    leds[L] = CRGB(0, 0, 0);
-    L = Serial.parseInt();
-    leds[L] = CRGB(0, 255, 0);
-    FastLED.show();
+  for (int L = 0; L < lct; L++) {
+
+    //rainbow inward circle
+    leds[L] = CHSV(millis() / 10 + 3 * sqrt(sq(coords[L][x] - cx) + sq(coords[L][y] - cy)), 255, 100);
+
+    //inward hard edge circle
+    //    if (((byte)(millis() / 2 + 3 * sqrt(sq(coords[L][x] - cx) + sq(coords[L][y] - cy)))) < 20) {
+    //      leds[L] = CRGB(255, 0, 255);
+    //    } else {
+    //      leds[L] = CRGB(0, 0, 0);
+    //    }
+
+
   }
+  FastLED.show();
 }
